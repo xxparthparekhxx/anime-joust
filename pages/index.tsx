96369ec2ecf1ecdef1d2485e88dc18ac3a0c3c17
xxparthparekhxx <xx.parthparekh.xx@gmail.com>
2023-01-02 03:50:29 +0530

    completed the basic ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className=''>
-      Hello World
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-center' >Which Pokemon is Roundest</div>
+      <div className='p-2'>
+
+      <div className='border rounded p-8 flex justify-between max-w-2xl items-center' >
+        <div className='bg-white w-16 h-16'></div>
+        <div className='p-8'>VS</div>
+        <div className='bg-white w-16 h-16'></div>
+      </div>
+      </div>
     </div>
   )
 }
